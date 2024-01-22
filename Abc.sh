c6d6b3d483584cca355b281f9d0a0f547995b9cc
rgbstroke21 <75511592+rgbstroke21@users.noreply.github.com>
2024-01-22 17:05:14 +0530

    Update Abc.sh

--- Abc.sh
+++ Abc.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Define HDFS paths
+input_path="hdfs://your_input_path.csv.gz"
+output_path="hdfs://your_output_path.csv"
+
+# Use zcat to decompress and copy to a new CSV file in HDFS
+zcat <(hadoop fs -cat $input_path) | hadoop fs -put - $output_path
+
+
+
+
 # Define HDFS paths
 input_path="hdfs://your_input_path.csv"
 output_path="hdfs://your_output_path.csv"
