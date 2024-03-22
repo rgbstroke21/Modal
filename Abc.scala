8c0958264bfdeb432bd755c049a580bad87ed909
rgbstroke21 <75511592+rgbstroke21@users.noreply.github.com>
2024-03-22 16:54:31 +0530

    Update Abc.scala

--- Abc.scala
+++ Abc.scala
@@ -1,6 +1,14 @@
 import org.apache.spark.sql.functions._
 
 // Assuming your DataFrame is named 'df' and the column containing the array of structs is named 'arrayOfStructs'
-val countNullA = df.withColumn("null_a_count", size(filter($"arrayOfStructs", struct => struct("a").isNull)))
+
+// Explode the array column
+val explodedDF = df.withColumn("exploded", explode($"arrayOfStructs"))
+
+// Add a column containing 1 for null "a" values and 0 otherwise
+val nullACountDF = explodedDF.withColumn("null_a_count", when($"exploded.a".isNull, 1).otherwise(0))
+
+// Group by the original DataFrame and sum the counts to get the total count of null "a" values
+val countNullA = nullACountDF.groupBy().agg(sum($"null_a_count").alias("null_a_count"))
 
 countNullA.show()
