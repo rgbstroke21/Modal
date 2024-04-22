41f1eb997f4e8eea8df75429c3c10f0ca105dec0
rgbstroke21 <75511592+rgbstroke21@users.noreply.github.com>
2024-04-22 12:44:12 +0530

    Create Testcase.scala

---
+++ Testcase.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+class ParquetToSequenceFileTest extends FunSuite with BeforeAndAfterAll {
+  
+  @transient private var spark: SparkSession = _
+  
+  override def beforeAll(): Unit = {
+    val conf = new SparkConf().setAppName("ParquetToSequenceFileTest").setMaster("local[2]")
+    spark = SparkSession.builder().config(conf).getOrCreate()
+  }
+  
+  override def afterAll(): Unit = {
+    if (spark != null) {
+      spark.stop()
+    }
+  }
+
+  test("Parquet to SequenceFile Conversion Test") {
+    val inputPath = "path/to/input/parquet"
+    val outputPath = "path/to/output/sequence"
+    
+    val inputDF = spark.read.parquet(inputPath)
+    
+    inputDF.rdd.map(row => (row.getAs[String]("key"), row.getAs[String]("value")))
+              .saveAsSequenceFile(outputPath)
+    
+    val outputRDD = spark.sparkContext.sequenceFile[String, String](outputPath)
+    val outputCount = outputRDD.count()
+    
+    assert(outputCount == inputDF.count(), "Number of records in input and output should match")
+  }
+}
