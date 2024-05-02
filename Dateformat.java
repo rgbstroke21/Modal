25cd3ef792a633f91b6325e4e59dae2b5d2826a5
rgbstroke21 <75511592+rgbstroke21@users.noreply.github.com>
2024-05-02 15:05:30 +0530

    Update Dateformat.java

--- Dateformat.java
+++ Dateformat.java
@@ -7,7 +7,7 @@ public class StringToEpochTime {
         String dateString = "2024-05-02T12:30:45:123Z";
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SSSZ");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date date = dateFormat.parse(dateString);
+        Date date = dateFormat.parse(dateString.replaceAll("(\\.\\d+)Z", "Z")); // Correcting milliseconds format
         long epochTime = date.getTime();
         System.out.println("Epoch Time: " + epochTime);
     }
