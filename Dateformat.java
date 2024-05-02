780cb87671de357eb24042c51f0a53464e6601f3
rgbstroke21 <75511592+rgbstroke21@users.noreply.github.com>
2024-05-02 14:51:46 +0530

    Update Dateformat.java

--- Dateformat.java
+++ Dateformat.java
@@ -2,18 +2,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class Main {
-    public static void main(String[] args) {
-        String timestamp = "2023-10-03T14:30Z";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        
-        try {
-            Date date = sdf.parse(timestamp);
-            long epochTime = date.getTime() / 1000; // Convert milliseconds to seconds
-            System.out.println("Epoch time: " + epochTime);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class StringToEpochTime {
+    public static void main(String[] args) throws Exception {
+        String dateString = "2024-05-02T12:30:45:123Z";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SSSZ");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = dateFormat.parse(dateString);
+        long epochTime = date.getTime();
+        System.out.println("Epoch Time: " + epochTime);
     }
 }
